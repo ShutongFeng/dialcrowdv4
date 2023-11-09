9cb722e29a7c940911760158fb44d580270f4a00
Hsien-Chin Lin <linh@hhu.de>
2023-11-09 13:23:13 +0100

    wip

--- src/configs.js
+++ src/configs.js
@@ -1,3 +1,3 @@
 export const serverUrl = process.env.NODE_ENV === 'development' ? 'http://34.32.143.76:3040' : process.env.serverUrl
-export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : process.env.clientUrl
+export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : process.env.PUBLIC_URL
 
