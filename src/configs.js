ecfe193434185ed77d65f258527c7a9fd6509732
Shutong Feng <shutong.feng@hhu.de>
2024-02-07 17:09:12 +0100

    remove the slash

--- src/configs.js
+++ src/configs.js
@@ -1,3 +1,3 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://dialcrowd-server-v3-0ddb228517b3.herokuapp.com' : "https://dialcrowd-server-dev-837f73c10566.herokuapp.com/"
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://dialcrowd-server-v3-0ddb228517b3.herokuapp.com' : "https://dialcrowd-server-dev-837f73c10566.herokuapp.com"
 export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : process.env.PUBLIC_URL
 
