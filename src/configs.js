4eba9d616bf0ab6fdf019835d1daf31968783399
Carel van Niekerk <40663106+carelvniekerk@users.noreply.github.com>
2024-02-07 15:20:48 +0100

    Update configs.js

--- src/configs.js
+++ src/configs.js
@@ -1,3 +1,3 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://dialcrowd-server-v3-0ddb228517b3.herokuapp.com' : "https://dialcrowd-server-v3-0ddb228517b3.herokuapp.com"
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://dialcrowd-server-v3-0ddb228517b3.herokuapp.com' : process.env.serverUrl
 export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : process.env.PUBLIC_URL
 
