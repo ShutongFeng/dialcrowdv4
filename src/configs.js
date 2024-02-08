c142fa734db9831d9f4ecf9d678bab132c279ecc
Carel van Niekerk <vniekerk.carel@gmail.com>
2024-02-08 11:33:52 +0100

    serverUrl from env

--- src/configs.js
+++ src/configs.js
@@ -1,3 +1,3 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://dialcrowd-server-5264df11cc57.herokuapp.com' : process.env.serverUrl
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://dialcrowd-server-5264df11cc57.herokuapp.com' : process.env.REACT_APP_SERVER_URL
 export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : process.env.PUBLIC_URL
 
