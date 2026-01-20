1651a761a7686e39df9e99828064bd72b981f0c0
Shutong Feng <shutong.feng@hhu.de>
2026-01-20 11:43:50 +0100

    update server url

--- src/configs.js
+++ src/configs.js
@@ -1,2 +1,2 @@
-export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://luster-human-eval-db245a0b4a24.herokuapp.com/' : process.env.REACT_APP_SERVER_URL
+export const serverUrl = process.env.NODE_ENV === 'development' ? 'https://luster-human-eval-db245a0b4a24.herokuapp.com' : process.env.REACT_APP_SERVER_URL
 export const clientUrl = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3000' : process.env.PUBLIC_URL
